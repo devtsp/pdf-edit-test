5a5d876fcedcf1318d653a038c5d89234d70d0f5
Tomas Paez <paeztms@gmail.com>
2023-02-05 16:20:03 -0300

    add "sign" button to float div over iframe and track coordinates of mouse-down cursor

--- components/canvas.js
+++ components/canvas.js
@@ -1,42 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PDFDocument } from 'pdf-lib';
 
 const Canvas = ({ initialDoc = 'Acknowledgement HIPAA.pdf' }) => {
+	// canvas events
 	const canvasRef = React.useRef();
-	const [isDrawing, setIsDrawing] = React.useState(false);
+	const [canvasIsDrawing, setCanvasIsDrawing] = React.useState(false);
+	const [canvasCoordinates, setCanvasCoordinates] = React.useState();
+
+	const [isSigning, setIsSigning] = React.useState(false);
+
 	const [pdfInfo, setPdfInfo] = React.useState([]);
 	const [convertedPdf, setConvertedPdf] = React.useState();
 
 	React.useEffect(() => {
-		canvasRef.current.onmousedown = function () {
-			setIsDrawing(true);
-		};
+		if (canvasRef.current) {
+			canvasRef.current.onmousedown = function () {
+				setCanvasIsDrawing(true);
+			};
 
-		canvasRef.current.onmouseup = function () {
-			setIsDrawing(false);
-		};
+			canvasRef.current.onmouseup = function () {
+				setCanvasIsDrawing(false);
+			};
+		}
 
 		getPdfInfo();
-	}, []);
+	}, [canvasRef.current, isSigning]);
 
 	async function getPdfInfo() {
 		const formPdfBytes = await fetch(initialDoc).then(res => res.arrayBuffer());
 		const pdfDoc = await PDFDocument.load(formPdfBytes);
 		const { width, height } = pdfDoc.getPage(0).getSize();
 		setPdfInfo({ page: 0, height, width });
-		console.log('page h: ', height, 'page w:', width);
 	}
 
 	function handleMouseMovement(e) {
-		console.log('trigger');
 		const rect = e.target.getBoundingClientRect();
-		const x = e.clientX - rect.left;
+		const x = (e.clientX - rect.left).toFixed(0);
 		const y = e.clientY - rect.top;
 		const porcentualPositionX = (x * 100) / rect.width;
 		const porcentualPositionY = (y * 100) / rect.height;
-		if (isDrawing) {
-			console.log(
-				'Left: %' + porcentualPositionX + ', Top: %' + porcentualPositionY
+		if (canvasIsDrawing) {
+			setCanvasCoordinates(
+				`Left: ${x} (%${porcentualPositionX.toFixed(
+					2
+				)}), Top: ${y} (%${porcentualPositionY.toFixed(2)})`
 			);
 		}
 	}
@@ -54,21 +61,31 @@ const Canvas = ({ initialDoc = 'Acknowledgement HIPAA.pdf' }) => {
 	// }
 
 	return (
-		<div style={{ display: 'flex' }}>
-			<div
-				style={{
-					border: '1px solid black',
-					height: pdfInfo.height,
-					width: pdfInfo.width,
-					position: 'relative',
-					padding: '10px',
-					boxSizing: 'content-box',
-				}}
-				onMouseMove={handleMouseMovement}
-				ref={canvasRef}
-			>
-				<iframe
-					src={initialDoc + '#toolbar=0&view=Fit'}
+		<div style={{ display: 'flex', justifyContent: 'center' }}>
+			<div>
+				<div style={{ display: 'flex', padding: '10px 0' }}>
+					<span>
+						Page h: {pdfInfo.height}, Page w: {pdfInfo.width}
+					</span>
+					<button
+						onClick={() => setIsSigning(prev => !prev)}
+						style={{
+							color: isSigning ? 'white' : 'black',
+							backgroundColor: isSigning ? 'red' : 'white',
+							marginLeft: 'auto',
+						}}
+					>
+						{isSigning ? 'SIGNING' : 'SIGN'}
+					</button>
+				</div>
+				<span>
+					{isSigning
+						? canvasIsDrawing
+							? canvasCoordinates
+							: 'Waiting mouse-down event'
+						: ''}
+				</span>
+				<div
 					style={{
 						border: 'none',
 						margin: 'auto',
@@ -76,18 +93,37 @@ const Canvas = ({ initialDoc = 'Acknowledgement HIPAA.pdf' }) => {
 						height: pdfInfo.height,
 						width: pdfInfo.width,
 					}}
-					// ref={canvasRef}
-				></iframe>
+				>
+					{isSigning ? (
+						<div
+							ref={canvasRef}
+							onMouseMove={handleMouseMovement}
+							style={{
+								display: 'flex',
+								alignItems: 'center',
+								justifyContent: 'center',
+								margin: 'auto',
+								height: pdfInfo.height,
+								width: pdfInfo.width,
+								backgroundColor: 'red',
+								opacity: '0.1',
+								position: 'absolute',
+								zIndex: '100',
+							}}
+						></div>
+					) : null}
+					<iframe
+						src={initialDoc + '#toolbar=0&view=Fit'}
+						style={{
+							border: 'none',
+							height: pdfInfo.height,
+							width: pdfInfo.width,
+							pointerEvents: isSigning ? 'none' : 'all',
+							userSelect: 'none',
+						}}
+					></iframe>
+				</div>
 			</div>
-			<div
-				onMouseMove={handleMouseMovement}
-				style={{
-					border: '1px solid black',
-					margin: 'auto',
-					height: pdfInfo.height,
-					width: pdfInfo.width,
-				}}
-			></div>
 		</div>
 	);
 };
